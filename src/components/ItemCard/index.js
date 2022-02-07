b1293d5a32c948534bc20d450354f64055a7544a
Adam <adamskoullos@gmail.com>
2022-02-07 07:42:33 +0000

    fix link bug

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -12,7 +12,12 @@ import ItemFooter from "../ItemFooter";
 const ItemCard = ({ item, i }) => {
   return (
     <Grid item xs={12}>
-      <a href={item.link} style={{ textDecoration: "none" }} target="_blank">
+      <a
+        href={item.link}
+        style={{ textDecoration: "none" }}
+        target="_blank"
+        rel="noreferrer"
+      >
         <Card variant="outlined" sx={{ height: "100%" }}>
           <CardActionArea>
             <CardHeader title={item.title} sx={{ color: "goldenrod" }} />
