e485761d2b891ffafe6513eaf2cd9ef60cf822a3
Adam <adamskoullos@gmail.com>
2022-02-12 10:23:23 +0000

    comment out in progress but not used code ready for build

--- src/components/FilterTags/index.jsx
+++ src/components/FilterTags/index.jsx
@@ -1,5 +1,4 @@
 import {
-  autocompleteClasses,
   Checkbox,
   FormControl,
   InputLabel,
--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 import { Grid, Typography } from "@mui/material";
 import ItemCard from "../../components/ItemCard";
