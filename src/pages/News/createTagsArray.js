64abab8bf6789d8190710688cb3f4007fc14d35a
Adam <adamskoullos@gmail.com>
2022-02-13 09:21:07 +0000

    fix build bug

--- src/pages/News/createTagsArray.js
+++ src/pages/News/createTagsArray.js
@@ -1,7 +1,7 @@
 const createTagsArray = (cryptoNews) => {
   const tagsArr = [];
   const arrayOfTagsArrays = cryptoNews.map((article) => article.tags);
-  arrayOfTagsArrays.map((tags) => {
+  arrayOfTagsArrays.forEach((tags) => {
     tags.forEach((tag) => {
       if (!tagsArr.includes(tag.name)) {
         tagsArr.push(tag.name);
